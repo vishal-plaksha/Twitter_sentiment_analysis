e6c4329d9432b0fadf8bafb56c35dfb839571ad1
Vishal Pandey <vishal@gnani.ai>
2020-11-13 14:24:05 +0530

    flask app added

---
+++ get_sentiment.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+from joblib import load
+from get_tweets import get_related_tweets
+
+
+pipeline = load("text_classification.joblib")
+
+
+def requestResults(name):
+    tweets = get_related_tweets(name)
+    tweets['prediction'] = pipeline.predict(tweets['tweet_text'])
+    del tweets['created_at']
+    del tweets['tweet_id']
+    return  tweets
+
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/', methods=['POST', 'GET'])
+def get_data():
+    if request.method == 'POST':
+        user = request.form['search']
+        return redirect(url_for('success', name=user))
+
+
+@app.route('/success/<name>')
+def success(name):
+    return "<xmp>" + str(requestResults(name)) + " </xmp> "
+
+
+if __name__ == '__main__' :
+    app.run(debug=True)
---
+++ get_tweets.py
@@ -0,0 +1,40 @@
+import tweepy
+import time
+import pandas as pd
+pd.set_option('display.max_colwidth', 1000)
+
+# api key
+api_key = "4vEUQjvdrzVy9Kmzep1Y4l9zR"
+# api secret key
+api_secret_key = "VMvrUbScT96NYOtmC0mmDWmeQapjNv1g8CUPfchrL9Zi0x8d7f"
+# access token
+access_token = "1000392397308575744-wQYSYPfFVSYtXhvxdTmoUOvdtBD6R8"
+# access token secret
+access_token_secret = "toG3h9ncJGFAFdeqrtc6sm3KJhS13q031pj56XAttJDW4"
+
+authentication = tweepy.OAuthHandler(api_key, api_secret_key)
+authentication.set_access_token(access_token, access_token_secret)
+api = tweepy.API(authentication, wait_on_rate_limit=True)
+
+def get_related_tweets(text_query):
+    # list to store tweets
+    tweets_list = []
+    # no of tweets
+    count = 5
+    try:
+        # Pulling individual tweets from query
+        for tweet in api.search(q=text_query, count=count):
+            #print(tweet.text)
+            # Adding to list that contains all tweets
+            tweets_list.append({'created_at': tweet.created_at,
+                                'tweet_id': tweet.id,
+                                'tweet_text': tweet.text})
+        return pd.DataFrame.from_dict(tweets_list)
+
+    except BaseException as e:
+        print('failed on_status,', str(e))
+        time.sleep(3)
+
+
+# data= get_related_tweets("donald trump")
+# print(data.info())
